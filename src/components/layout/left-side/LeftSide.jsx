0f72536ff72c8a7b72a3b7b4fecc1b0ad3da72d2
Zagir <zaynalov005@mail.ru>
2023-10-03 12:54:47 +0300

    bug leftside

--- src/components/layout/left-side/LeftSide.jsx
+++ src/components/layout/left-side/LeftSide.jsx
@@ -1,25 +1,47 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from "./LeftSide.module.scss";
 import { menu } from "./menu.data";
 import cn from "clsx";
+import { useState } from "react";
+import { TbArrowRight } from "react-icons/tb";
 
 const LeftSide = () => {
+  const [isActive, setIsActive] = useState(true);
   const { pathname } = useLocation();
   return (
-    <div className={styles.leftside}>
-      <div className={styles.logo}>
+    <div
+      className={cn(styles.leftside, {
+        [styles.leftsideActive]: isActive,
+      })}
+    >
+      <button
+        onClick={() => setIsActive(!isActive)}
+        className={cn(styles.activeButton, {
+          [styles.isActiveButton]: isActive,
+        })}
+      >
+        <TbArrowRight fontSize={30} />
+      </button>
+      <Link to={"/"} className={styles.logo}>
         <img src="./logo.png" alt="" />
-      </div>
+        {isActive && (
+          <div className={styles.logoText}>
+            <span>Dapplets Project</span>
+            <span>.</span>
+          </div>
+        )}
+      </Link>
       {menu.map((el) => (
-        <a
-          href={el.path}
+        <Link
+          to={el.path}
           key={el.path}
           className={cn(styles.nav, {
             [styles.active]: pathname === el.path,
           })}
         >
           <img src={el.icon} alt={el.icon} />
-        </a>
+          {isActive && <div>{el.name}</div>}
+        </Link>
       ))}
     </div>
   );
